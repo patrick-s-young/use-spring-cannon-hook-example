350d8be84af8a275b8578b8211ad0d25fed85c58
patrick-s-young <patrick.shaffer.young@gmail.com>
2022-08-03 14:53:42 -0700

    useSpring inputs via leva and zustand @USP-002

--- src/Ground/configs/index.js
+++ src/Ground/configs/index.js
@@ -1,12 +1,12 @@
 export const compoundShapes = [
-  { type: 'Cylinder', args: [5, 5, 1, 24], position: [0, -.5, 0], color: 'orange'},
-  { type: 'Sphere', args: [.25], position: [4.5 * Math.cos(0),              0,  4.5 * Math.sin(0)             ], color: 'yellow' },
-  { type: 'Sphere', args: [.20], position: [4.5 * Math.cos(Math.PI * .125), 0,  4.5 * Math.sin(Math.PI * .125)], color: 'yellow' },
-  { type: 'Sphere', args: [.15], position: [4.5 * Math.cos(Math.PI * .25),  0,  4.5 * Math.sin(Math.PI * .25) ], color: 'yellow' },
-  { type: 'Sphere', args: [.25], position: [4.5 * Math.cos(Math.PI * .5),   0,  4.5 * Math.sin(Math.PI * .5)  ], color: 'yellow' },
-  { type: 'Sphere', args: [.25], position: [4.5 * Math.cos(Math.PI * 1),    0,  4.5 * Math.sin(Math.PI * 1)   ], color: 'yellow' },
-  { type: 'Sphere', args: [.45], position: [4.5 * Math.cos(Math.PI * 1.5),  0,  4.5 * Math.sin(Math.PI * 1.5) ], color: 'yellow' },
-  { type: 'Sphere', args: [.40], position: [4.5 * Math.cos(Math.PI * 1.565),  0,  4.5 * Math.sin(Math.PI * 1.565) ], color: 'yellow' },
-  { type: 'Sphere', args: [.30], position: [4.5 * Math.cos(Math.PI * 1.62),  0,  4.5 * Math.sin(Math.PI * 1.62) ], color: 'yellow' },
-  { type: 'Sphere', args: [.25], position: [4.5 * Math.cos(Math.PI * 1.66),  0,  4.5 * Math.sin(Math.PI * 1.66) ], color: 'yellow' }
+  { type: 'Cylinder', args: [5, 5, 1, 24], position: [0, -.5, 0], color: 'orange', key: 'C01'},
+  { type: 'Sphere', args: [.25], position: [4.5 * Math.cos(0),              0,  4.5 * Math.sin(0)             ], color: 'yellow', key: 'C0' },
+  { type: 'Sphere', args: [.20], position: [4.5 * Math.cos(Math.PI * .125), 0,  4.5 * Math.sin(Math.PI * .125)], color: 'yellow', key: 'C1' },
+  { type: 'Sphere', args: [.15], position: [4.5 * Math.cos(Math.PI * .25),  0,  4.5 * Math.sin(Math.PI * .25) ], color: 'yellow', key: 'C2' },
+  { type: 'Sphere', args: [.25], position: [4.5 * Math.cos(Math.PI * .5),   0,  4.5 * Math.sin(Math.PI * .5)  ], color: 'yellow', key: 'C3' },
+  { type: 'Sphere', args: [.25], position: [4.5 * Math.cos(Math.PI * 1),    0,  4.5 * Math.sin(Math.PI * 1)   ], color: 'yellow', key: 'C4' },
+  { type: 'Sphere', args: [.45], position: [4.5 * Math.cos(Math.PI * 1.5),  0,  4.5 * Math.sin(Math.PI * 1.5) ], color: 'yellow', key: 'C5' },
+  { type: 'Sphere', args: [.40], position: [4.5 * Math.cos(Math.PI * 1.565),0,  4.5 * Math.sin(Math.PI * 1.565)], color: 'yellow', key: 'C6' },
+  { type: 'Sphere', args: [.30], position: [4.5 * Math.cos(Math.PI * 1.62), 0,  4.5 * Math.sin(Math.PI * 1.62) ], color: 'yellow', key: 'C7' },
+  { type: 'Sphere', args: [.25], position: [4.5 * Math.cos(Math.PI * 1.66), 0,  4.5 * Math.sin(Math.PI * 1.66) ], color: 'yellow', key: 'C8' }
 ];
--- src/Robot/index.js
+++ src/Robot/index.js
@@ -1,13 +1,13 @@
+// react
 import { useRef } from "react";
+// cannon hooks
 import { useSphere, useSpring } from '@react-three/cannon';
+// zustand
+import { useStore } from '../store';
 
 export const Robot = () => {
-  // spring connecting robot's head to its torso
-  const [headSpring, torsoSpring] = useSpring(useRef(null), useRef(null), {
-    damping: 1,
-    restLength: .5,
-    stiffness: 100,
-  })
+  // useSpring values
+  const { damping, restLength, stiffness } = useStore(state => state);
 
   // robot head
   const [headRef] = useSphere(() => ({ 
@@ -16,17 +16,24 @@ export const Robot = () => {
     position: [0, 3, 4.5],
     linearFactor: [0, 1, 0],
     angularFactor: [0, 1, 0]
-  }), headSpring)
+  }), useRef(null))
 
   // robot torso
   const [torsoRef] = useSphere(() => ({
     args: [.25],
     position: [0, 1.5, 4.5],
     mass: 1,
-    linearFactor: [0, 1, 0],
-  }), torsoSpring);
+    linearFactor: [0, 1, 0]
+  }), useRef(null));
 
-  // robot feet
+  // spring connecting robot's head to its torso
+  const [headSpring, torsoSpring] = useSpring(headRef, torsoRef, {
+    damping,
+    restLength,
+    stiffness,
+  }, [damping, restLength, stiffness]);
+
+  // robot 'feet'
   const [feefRef] = useSphere(() => ({
     args: [.40],
     position: [0, .5, 4.5],
@@ -36,11 +43,11 @@ export const Robot = () => {
 
   return (
     <>
-      <mesh ref={headRef}>
+      <mesh ref={headSpring}>
         <sphereBufferGeometry args={[0.125]} />
         <meshNormalMaterial />
       </mesh>
-      <mesh ref={torsoRef}>
+      <mesh ref={torsoSpring}>
       <sphereBufferGeometry args={[0.25]} />
         <meshNormalMaterial />
       </mesh>
